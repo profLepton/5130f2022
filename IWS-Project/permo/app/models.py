29ee4fe9403d1e93e0e798c06a0ae02cd3bd368b
Akshay Kolli <proflepton@gmail.com>
2022-11-22 21:55:50 -0500

    Week 4 Update

--- IWS-Project/permo/app/models.py
+++ IWS-Project/permo/app/models.py
@@ -8,19 +8,18 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-request = db.Table('requests',
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('status', db.Boolean, index = True),
-    db.Column('student_id', db.Integer, db.ForeignKey('user.id'), index=True),
-    db.Column('course_id', db.Integer, db.ForeignKey('college_course.id'), index=True)
-)   
+# request = db.Table('requests',
+#     db.Column('id', db.Integer, primary_key=True),
+#     db.Column('status', db.Boolean, index = True),
+#     db.Column('student_id', db.Integer, db.ForeignKey('user.id'), index=True),
+#     db.Column('course_id', db.Integer, db.ForeignKey('college_course.id'), index=True)
+# )   
 
 
 class User(UserMixin, db.Model):
     firstName = db.Column(db.String(64), index=True)
     lastName = db.Column(db.String(64), index=True)
     id = db.Column(db.Integer, primary_key=True)
-    is_professor = db.Column(db.Boolean)
     username = db.Column(db.String(64), index=True, unique=True, )
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -30,11 +29,11 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime, default=datetime.utcnow  )
     verified = db.Column(db.Boolean, default=False)
     verified_on = db.Column(db.DateTime, default=datetime.utcnow)
-    requests = db.relationship('User', 
-    secondary = request, 
-    primaryjoin=(request.c.course_id == id),
-    secondaryjoin=(request.c.student_id == id),
-    backref=db.backref('requests', lazy='dynamic'), lazy='dynamic')
+    # requests = db.relationship('User', 
+    # secondary = request, 
+    # primaryjoin=(request.c.course_id == id),
+    # secondaryjoin=(request.c.student_id == id),
+    # backref=db.backref('requests', lazy='dynamic'), lazy='dynamic')
 
 
 
@@ -73,6 +72,15 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+class Professor(User):
+     courses = db.relationship('CollegeCourse', backref='professor', lazy='dynamic')
+
+class Student(User):
+    pass
+
+
+
+
 ### Work in progress.
 
 ### timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) 
@@ -82,12 +90,12 @@ class CollegeCourse(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     course_name = db.Column(db.String(20))
     professor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    permission_numbers = db.relationship('permission_numbers', backref='course')
-    requests = db.relationship('college_course', 
-    secondary = request, 
-    primaryjoin=(request.c.course_id == id),
-    secondaryjoin=(request.c.student_id == id),
-    backref=db.backref('requests', lazy='dynamic'), lazy='dynamic')
+    # permission_numbers = db.relationship('permission_numbers', backref='course')
+    # requests = db.relationship('college_course', 
+    # secondary = request, 
+    # primaryjoin=(request.c.course_id == id),
+    # secondaryjoin=(request.c.student_id == id),
+    # backref=db.backref('requests', lazy='dynamic'), lazy='dynamic')
 
     
 
@@ -95,13 +103,13 @@ class CollegeCourse(db.Model):
     def __repr__(self):
         return '<CollegeCourse {}'.format(self.course_name)
     
-permission_numbers = db.Table('permission_numbers',
-    db.Column('id', db.Integer, primary_key = True),
-    db.Column('permission_number', db.Integer),
-    db.Column('Assigned', db.Boolean, index=True),
-    db.Column('course_name', db.String, db.ForeignKey('college_course.course_name'), index=True),
-    db.Column('student_id', db.Integer, db.ForeignKey('user.id'), index=True)
-)
+# permission_numbers = db.Table('permission_numbers',
+#     db.Column('id', db.Integer, primary_key = True),
+#     db.Column('permission_number', db.Integer),
+#     db.Column('Assigned', db.Boolean, index=True),
+#     db.Column('course_name', db.String, db.ForeignKey('college_course.course_name'), index=True),
+#     db.Column('student_id', db.Integer, db.ForeignKey('user.id'), index=True)
+# )
 
 
 
--- IWS-Project/permo/permo.py
+++ IWS-Project/permo/permo.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User
+from app.models import User, CollegeCourse
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'CollegeCourse': CollegeCourse}
